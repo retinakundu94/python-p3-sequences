e0aa243e2b48bc8f4167da66c15170f8c4ed106a
Retina Kundu <retinakundu94@gmail.com>
2024-04-28 15:51:38 -0400

    add changes

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])  # Updated to print an empty list for non-positive length
+    elif length == 1:
+        print([0])
+    else:
+        fibonacci_list = [0, 1]
+        for i in range(2, length):
+            next_number = fibonacci_list[-1] + fibonacci_list[-2]
+            fibonacci_list.append(next_number)
+        print(fibonacci_list)
